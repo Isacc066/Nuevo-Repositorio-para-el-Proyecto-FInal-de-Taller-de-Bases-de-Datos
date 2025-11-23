349bf891237134785e5ae1455779970d1a4f970a
Isacc066 <s23120207@alumnos.itsur.edu.mx>
2025-11-22 20:21:59 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_Final_PuntoDeVentaDeLibreria/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Final_PuntoDeVentaDeLibreria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
