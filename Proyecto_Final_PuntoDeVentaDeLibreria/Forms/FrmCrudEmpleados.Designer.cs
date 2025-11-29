cbf3c0477cddf6920e5a145decb83fe7d9cb8dcc
Isacc066 <s23120207@alumnos.itsur.edu.mx>
2025-11-29 17:45:28 -0600

    FrmCrudEmpleados agregado

---
+++ Proyecto_Final_PuntoDeVentaDeLibreria/Forms/FrmCrudEmpleados.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Proyecto_Final_PuntoDeVentaDeLibreria.Forms
+{
+    partial class FrmCrudEmpleados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAgregar = new Button();
+            btnEditar = new Button();
+            btnEliminar = new Button();
+            btnGuardar = new Button();
+            btnCancelar = new Button();
+            txtNombre = new TextBox();
+            txtApellido = new TextBox();
+            txtTelefono = new TextBox();
+            txtDireccion = new TextBox();
+            dgvEmpleados = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvEmpleados).BeginInit();
+            SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(75, 36);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(94, 29);
+            btnAgregar.TabIndex = 0;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(75, 109);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(94, 29);
+            btnEditar.TabIndex = 1;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            btnEditar.Click += btnEditar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(75, 180);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(94, 29);
+            btnEliminar.TabIndex = 2;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(75, 249);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(94, 29);
+            btnGuardar.TabIndex = 3;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(75, 320);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(94, 29);
+            btnCancelar.TabIndex = 4;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(227, 36);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(125, 27);
+            txtNombre.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(380, 36);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(125, 27);
+            txtApellido.TabIndex = 6;
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(526, 36);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(125, 27);
+            txtTelefono.TabIndex = 7;
+            // 
+            // txtDireccion
+            // 
+            txtDireccion.Location = new Point(663, 38);
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.Size = new Size(125, 27);
+            txtDireccion.TabIndex = 8;
+            // 
+            // dgvEmpleados
+            // 
+            dgvEmpleados.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEmpleados.Location = new Point(430, 180);
+            dgvEmpleados.Name = "dgvEmpleados";
+            dgvEmpleados.RowHeadersWidth = 51;
+            dgvEmpleados.Size = new Size(300, 188);
+            dgvEmpleados.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(284, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 10;
+            label1.Text = "Nombre";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(430, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 20);
+            label2.TabIndex = 11;
+            label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(571, 10);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 20);
+            label3.TabIndex = 12;
+            label3.Text = "Telefono";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(714, 12);
+            label4.Name = "label4";
+            label4.Size = new Size(72, 20);
+            label4.TabIndex = 13;
+            label4.Text = "Direccion";
+            // 
+            // FrmCrudEmpleados
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dgvEmpleados);
+            Controls.Add(txtDireccion);
+            Controls.Add(txtTelefono);
+            Controls.Add(txtApellido);
+            Controls.Add(txtNombre);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnGuardar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnEditar);
+            Controls.Add(btnAgregar);
+            Name = "FrmCrudEmpleados";
+            Text = "FrmCrudEmpleados";
+            ((System.ComponentModel.ISupportInitialize)dgvEmpleados).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAgregar;
+        private Button btnEditar;
+        private Button btnEliminar;
+        private Button btnGuardar;
+        private Button btnCancelar;
+        private TextBox txtNombre;
+        private TextBox txtApellido;
+        private TextBox txtTelefono;
+        private TextBox txtDireccion;
+        private DataGridView dgvEmpleados;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ Proyecto_Final_PuntoDeVentaDeLibreria/Forms/FrmCrudEmpleados.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Proyecto_Final_PuntoDeVentaDeLibreria.Models;
+using Proyecto_Final_PuntoDeVentaDeLibreria.DAO;
+
+namespace Proyecto_Final_PuntoDeVentaDeLibreria.Forms
+{
+    public partial class FrmCrudEmpleados : Form
+    {
+        private EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+        private bool esNuevo = false;      // Modo Nuevo
+        private int idEmpleadoSeleccionado; // Para editar
+
+        private void CargarEmpleados()
+        {
+            dgvEmpleados.DataSource = empleadoDAO.ListarTodos();
+
+            dgvEmpleados.Columns["IdEmpleado"].HeaderText = "ID";
+            dgvEmpleados.Columns["Nombre"].HeaderText = "Nombre";
+            dgvEmpleados.Columns["Apellido"].HeaderText = "Apellido";
+            dgvEmpleados.Columns["Telefono"].HeaderText = "Teléfono";
+            dgvEmpleados.Columns["Direccion"].HeaderText = "Dirección";
+            dgvEmpleados.Columns["NombreCompleto"].Visible = false;
+
+            dgvEmpleados.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtTelefono.Text = "";
+            txtDireccion.Text = "";
+        }
+
+        private void HabilitarFormulario(bool habilitar)
+        {
+            txtNombre.Enabled = habilitar;
+            txtApellido.Enabled = habilitar;
+            txtTelefono.Enabled = habilitar;
+            txtDireccion.Enabled = habilitar;
+
+            btnGuardar.Enabled = habilitar;
+            btnCancelar.Enabled = habilitar;
+
+            btnAgregar.Enabled = !habilitar;
+            btnEditar.Enabled = !habilitar;
+            btnEliminar.Enabled = !habilitar;
+        }
+
+        private void FrmCrudEmpleados_Load(object sender, EventArgs e)
+        {
+            CargarEmpleados();
+            HabilitarFormulario(false);
+        }
+        public FrmCrudEmpleados()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            esNuevo = true;
+            LimpiarFormulario();
+            HabilitarFormulario(true);
+            txtNombre.Focus();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dgvEmpleados.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione un empleado para editar.",
+                    "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            esNuevo = false;
+
+            Empleado emp = (Empleado)dgvEmpleados.SelectedRows[0].DataBoundItem;
+
+            idEmpleadoSeleccionado = emp.IdEmpleado;
+
+            txtNombre.Text = emp.Nombre;
+            txtApellido.Text = emp.Apellido;
+            txtTelefono.Text = emp.Telefono;
+            txtDireccion.Text = emp.Direccion;
+
+            HabilitarFormulario(true);
+            txtNombre.Focus();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvEmpleados.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Seleccione un empleado para eliminar.",
+                    "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            Empleado emp = (Empleado)dgvEmpleados.SelectedRows[0].DataBoundItem;
+
+            DialogResult resp = MessageBox.Show(
+                $"¿Eliminar a {emp.NombreCompleto}?",
+                "Confirmar",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (resp == DialogResult.Yes)
+            {
+                if (empleadoDAO.Eliminar(emp.IdEmpleado))
+                {
+                    MessageBox.Show("Empleado eliminado correctamente.");
+                    CargarEmpleados();
+                }
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text.Trim() == "" ||
+        txtApellido.Text.Trim() == "" ||
+        txtTelefono.Text.Trim() == "" ||
+        txtDireccion.Text.Trim() == "")
+            {
+                MessageBox.Show("Complete todos los campos.");
+                return;
+            }
+
+            Empleado emp = new Empleado
+            {
+                Nombre = txtNombre.Text,
+                Apellido = txtApellido.Text,
+                Telefono = txtTelefono.Text,
+                Direccion = txtDireccion.Text
+            };
+
+            bool resultado;
+
+            if (esNuevo)
+            {
+                resultado = empleadoDAO.Insertar(emp);
+                MessageBox.Show("Empleado agregado correctamente.");
+            }
+            else
+            {
+                emp.IdEmpleado = idEmpleadoSeleccionado;
+                resultado = empleadoDAO.Actualizar(emp);
+                MessageBox.Show("Empleado actualizado correctamente.");
+            }
+
+            if (resultado)
+            {
+                CargarEmpleados();
+                LimpiarFormulario();
+                HabilitarFormulario(false);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+            HabilitarFormulario(false);
+        }
+    }
+}
