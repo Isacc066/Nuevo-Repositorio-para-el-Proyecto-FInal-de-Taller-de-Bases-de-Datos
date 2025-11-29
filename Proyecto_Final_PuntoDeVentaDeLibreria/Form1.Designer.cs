2fbf66e3b00df697f3226a51dde42d85a1c410da
Isacc066 <s23120207@alumnos.itsur.edu.mx>
2025-11-29 10:03:13 -0600

    Agregué el botón Ingresar, los formularios MenuAdmin y MenuEmpleado, ya quedó el login

--- Proyecto_Final_PuntoDeVentaDeLibreria/Form1.Designer.cs
+++ Proyecto_Final_PuntoDeVentaDeLibreria/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             button1 = new Button();
             label1 = new Label();
+            txtUsuario = new TextBox();
+            txtContrasena = new TextBox();
+            button2 = new Button();
+            label2 = new Label();
             SuspendLayout();
             // 
             // button1
@@ -45,17 +49,55 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(503, 380);
+            label1.Location = new Point(222, 191);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(59, 20);
             label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Text = "Usuario";
+            label1.Click += label1_Click;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(185, 214);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(125, 27);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtContrasena
+            // 
+            txtContrasena.Location = new Point(185, 307);
+            txtContrasena.Name = "txtContrasena";
+            txtContrasena.Size = new Size(125, 27);
+            txtContrasena.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(199, 123);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Ingresar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += btnIngresar_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(210, 275);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Contraseña";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(txtContrasena);
+            Controls.Add(txtUsuario);
             Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form1";
@@ -68,5 +110,9 @@
 
         private Button button1;
         private Label label1;
+        private TextBox txtUsuario;
+        private TextBox txtContrasena;
+        private Button button2;
+        private Label label2;
     }
 }
--- Proyecto_Final_PuntoDeVentaDeLibreria/Form1.cs
+++ Proyecto_Final_PuntoDeVentaDeLibreria/Form1.cs
@@ -1,3 +1,4 @@
+using Proyecto_Final_PuntoDeVentaDeLibreria.Dao;
 using Proyecto_Final_PuntoDeVentaDeLibreria.DAO;
 
 namespace Proyecto_Final_PuntoDeVentaDeLibreria
@@ -22,6 +23,48 @@ namespace Proyecto_Final_PuntoDeVentaDeLibreria
             {
                 MessageBox.Show("Error: " + ex.Message);
             }
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            DAOUsuarios dao = new DAOUsuarios();
+
+            string usuario = txtUsuario.Text.Trim();
+            string contrasena = txtContrasena.Text.Trim();
+
+            if (usuario == "" || contrasena == "")
+            {
+                MessageBox.Show("Debe ingresar usuario y contraseña.");
+                return;
+            }
+
+            string rol = dao.Login(usuario, contrasena);
+
+            if (rol == null)
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos.");
+            }
+            else
+            {
+                MessageBox.Show("Bienvenido: " + rol);
+
+                if (rol == "admin")
+                {
+                    new FormMenuAdmin().Show();
+                }
+                else
+                {
+                    new FormMenuEmpleado().Show();
+                }
+
+                this.Hide();
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
