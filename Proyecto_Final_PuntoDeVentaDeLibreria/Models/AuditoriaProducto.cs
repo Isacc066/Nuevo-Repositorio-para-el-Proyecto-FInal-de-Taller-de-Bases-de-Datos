2292cf6f8e6d0a654747eddd45631e3d6b534efc
Isacc066 <s23120207@alumnos.itsur.edu.mx>
2025-12-02 23:06:54 -0600

    Clases AuditoriaProducto y AuditoriaDAO listas

---
+++ Proyecto_Final_PuntoDeVentaDeLibreria/Models/AuditoriaProducto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_PuntoDeVentaDeLibreria.Models
+{
+    internal class AuditoriaProducto
+    {
+        public int IdAuditoria { get; set; }
+        public DateTime FechaHora { get; set; }
+        public string Accion { get; set; }  // INSERT, UPDATE, DELETE
+        public int? IdProducto { get; set; }
+        public string Usuario { get; set; }
+        public string ValoresAnteriores { get; set; }
+        public string ValoresNuevos { get; set; }
+    }
+}
